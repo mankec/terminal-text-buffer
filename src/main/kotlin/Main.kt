59acb9490d63a3de5f98833b683dd9f7b416a340
Milan Kecman <milan.kecman270@gmail.com>
2026-03-13 19:41:41 +0100

    feat: add short demonstration

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -27,6 +27,57 @@ fun main() {
         else AnsiEffectPrompt.collectUserInput(
             "Choose text style: "
         )
-    val terminalBuffer = TerminalBuffer(scrollbackMaxSize)
+
+    val terminal = TerminalBuffer(scrollbackMaxSize)
         .setup(width, height, foregroundColor, backgroundColor, style)
+    val screen = terminal.screen
+
+    terminal.insert("I am in scrollback!")
+    terminal.enterNewLine()
+    terminal.insert("Cras vitae pharetra risus. Ut eget velit.")
+    terminal.enterNewLine()
+    terminal.insert("Fusce euismod non lacus et semper.")
+    terminal.enterNewLine()
+    terminal.insert("Quisque ut fringilla turpis. Curabitur in eros velit.")
+    terminal.enterNewLine()
+
+    screen.resetCursor()
+    val overwriteWithText = (
+        "I have been subsequently inserted. " +
+        "My job is to show you wrapped functionality. Might as well continue blabbering.."
+    )
+    overwriteWithText.forEach { terminal.write(it.toString()) }
+
+    screen.lines.forEachIndexed { idx, line ->
+        val flattenedRows = line.flatten()
+        if (idx == 1) {
+            println("Line $idx  ")
+            line.forEach { row ->
+                println(row.joinToString("") { it.render() })
+            }
+        } else if (line in screen.getNonFrozenLines()) {
+            println("Line $idx  ${flattenedRows.joinToString("") { it.render() } }")
+        } else {
+            println("Line $idx  ${flattenedRows.joinToString("") { 
+                it.foregroundColor = AnsiColor.WHITE 
+                it.backgroundColor = AnsiColor.BLACK 
+                it.render() 
+            } }")
+        }
+    }
+
+    println("")
+
+    println("------------------------------------")
+    println("SCREEN CONTENT AS STRING")
+    println(terminal.getScreenContentAsString())
+    println("------------------------------------")
+
+    println("")
+
+    println("------------------------------------")
+    println("ENTIRE CONTENT AS STRING")
+    println(terminal.getEntireContentAsString())
+    println("------------------------------------")
+
 }
